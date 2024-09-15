a855fc27affa1d4ca545869dd21ac65895b0f9dc
chankruze <chankruze@gmail.com>
2024-09-16 01:21:54 +0530

    fix css of movie card and booking page
    Signed-off-by: chankruze <chankruze@gmail.com>

--- frontend/src/components/Bookings/Booking.jsx
+++ frontend/src/components/Bookings/Booking.jsx
@@ -44,62 +44,95 @@ const Booking = () => {
           >
             Book TIckets Of Movie: {movie.title}
           </Typography>
-          <Box display={"flex"} justifyContent={"center"}>
-            <Box
-              display={"flex"}
-              justifyContent={"column"}
-              flexDirection="column"
-              paddingTop={3}
-              width="50%"
-              marginRight={"auto"}
-            >
-              <img
-                width="80%"
-                height={"300px"}
-                src={movie.posterUrl}
-                alt={movie.title}
-              />
-              <Box width={"80%"} marginTop={3} padding={2}>
-                <Typography paddingTop={2}>{movie.description}</Typography>
-                <Typography fontWeight={"bold"} marginTop={1}>
-                  Starrer:
-                  {movie.actors.map((actor) => " " + actor + " ")}
-                </Typography>
-                <Typography fontWeight={"bold"} marginTop={1}>
-                  Release Date: {new Date(movie.releaseDate).toDateString()}
-                </Typography>
-              </Box>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: {
+                xs: "1fr", // Single column on extra-small screens (mobile)
+                sm: "1fr 1fr", // Two columns on small screens (tablet)
+                md: "1fr 1fr 1fr", // Three columns on medium screens and above
+              },
+              padding: {
+                xs: 2, // Padding for extra-small screens (mobile)
+                sm: 4, // Padding for small screens (tablet)
+                md: 8, // Padding for medium screens and above
+              },
+              gap: {
+                xs: 2, // Gap for extra-small screens (mobile)
+                sm: 4, // Gap for small screens (tablet)
+                md: 8, // Gap for medium screens and above
+              },
+            }}
+          >
+            <img width="100%" src={movie.posterUrl} alt={movie.title} />
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+              <Typography variant="h5">{movie.description}</Typography>
+              {movie.actors.length > 0 ? (
+                <Typography>Starrer: {movie.actors.join(", ")}</Typography>
+              ) : null}
+              <Typography variant="body2">
+                Release Date: {new Date(movie.releaseDate).toDateString()}
+              </Typography>
             </Box>
-            <Box width={"50%"} paddingTop={3}>
-              <form onSubmit={handleSubmit}>
+            <Box>
+              <form
+                onSubmit={handleSubmit}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  flex: 1,
+                  gap: 8,
+                }}
+              >
                 <Box
-                  padding={5}
-                  margin={"auto"}
-                  display="flex"
-                  flexDirection={"column"}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
                   <FormLabel>Seat Number</FormLabel>
                   <TextField
                     name="seatNumber"
                     value={inputs.seatNumber}
                     onChange={handleChange}
-                    type={"number"}
+                    type="number"
                     margin="normal"
                     variant="standard"
+                    placeholder="i.e. 15"
                   />
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
                   <FormLabel>Booking Date</FormLabel>
                   <TextField
                     name="date"
-                    type={"date"}
+                    type="date"
                     margin="normal"
                     variant="standard"
                     value={inputs.date}
                     onChange={handleChange}
+                    defaultValue={new Date().toISOString().slice(0, 10)}
                   />
-                  <Button type="submit" sx={{ mt: 3 }}>
-                    Book Now
-                  </Button>
                 </Box>
+                <Button
+                  type="submit"
+                  sx={{
+                    bgcolor: "#1c0840",
+                    ":hover": {
+                      bgcolor: "#121217",
+                    },
+                    padding: 1,
+                    borderRadius: 2,
+                  }}
+                  variant="contained"
+                  fullWidth
+                >
+                  Book Now
+                </Button>
               </form>
             </Box>
           </Box>
--- frontend/src/components/Movies/MovieItem.jsx
+++ frontend/src/components/Movies/MovieItem.jsx
@@ -1,35 +1,60 @@
-import React from "react";
 import {
+  Button,
   Card,
   CardActions,
   CardContent,
   Typography,
-  Button,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
   return (
     <Card
       sx={{
+        display: "flex",
+        flexDirection: "column",
         margin: 2,
         width: 250,
-        height: 320,
-        borderRadius: 5,
+        borderRadius: 3,
         ":hover": {
           boxShadow: "10px 10px 20px #ccc",
         },
       }}
     >
-      <img width="100%" height={"50%"} src={posterUrl} alt="" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <img
+        width="100%"
+        height="350px"
+        src={posterUrl}
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <CardContent
+        sx={{
+          flex: 1,
+          padding: 2,
+        }}
+      >
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="div"
+          // sx={{
+          //   whiteSpace: "nowrap",
+          //   overflow: "hidden",
+          //   textOverflow: "ellipsis",
+          // }}
+        >
           {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {new Date(releaseDate).toDateString()}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions
+        sx={{
+          marginTop: "auto",
+        }}
+      >
         <Button
           LinkComponent={Link}
           to={`/booking/${id}`}
@@ -40,6 +65,8 @@ const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
             ":hover": {
               bgcolor: "#121217",
             },
+            padding: 1,
+            borderRadius: 2,
           }}
           variant="contained"
           fullWidth
