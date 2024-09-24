7e0adb663cf5066ca67a7384a9618f6286a85cdf
chankruze <chankruze@gmail.com>
2024-09-24 15:12:55 +0530

    update seeding script
    Signed-off-by: chankruze <chankruze@gmail.com>

--- backend/db/seed/transform.js
+++ backend/db/seed/transform.js
@@ -15,32 +15,34 @@ function transformData(moviesArray) {
   // Fixed adminId
   const adminId = "66e71ba97a4cf8884ecf078d";
 
-  // Transform each movie object
-  const ten = moviesArray.slice(-10);
+  const filteredMovies = moviesArray.filter(
+    (movie) =>
+      movie.title &&
+      movie.poster &&
+      movie.plot &&
+      movie.hasOwnProperty("released") &&
+      typeof movie.released.$date === "string"
+  );
 
-  return moviesArray
-    .slice(-2000)
-    .filter(
-      (movie) =>
-        movie.title &&
-        movie.poster &&
-        movie.plot &&
-        movie.hasOwnProperty("released") &&
-        movie.year > 2010
-    )
-    .map((movie) => ({
-      title: movie.title,
-      description: movie.plot, // Choose available description
-      releaseDate: movie.released.$date,
-      posterUrl: movie.poster, // Use poster or empty string if not present
-      adminId: adminId,
-      actors: movie.cast,
-    }));
+  // Shuffle the filtered movies
+  const shuffledMovies = shuffle(filteredMovies);
+
+  // Pick the first 1000 movies
+  const selectedMovies = shuffledMovies.slice(0, 1000);
+
+  return selectedMovies.map((movie) => ({
+    title: movie.title,
+    description: movie.plot, // Choose available description
+    releaseDate: movie.released.$date,
+    posterUrl: movie.poster, // Use poster or empty string if not present
+    adminId: adminId,
+    actors: movie.cast,
+  }));
 }
 
-// transform and shuffle
-const transformedMovies = shuffle(transformData(movies));
-console.log(`Transformed and shuffled ${transformedMovies.length} movies.`);
+// Transform and shuffle
+const transformedMovies = transformData(movies);
+console.log(`Transformed and selected ${transformedMovies.length} movies.`);
 
 // write to a file
 const filePath = path.join(__dirname, "movies.json");
